05851c16e1cc6a7d5336be0b128858e44ba16a2d
EndiRos <eetxebarrieta@gmail.com>
2025-10-07 14:13:21 +0200

    subo desciption y can

---
+++ src/openarm_can/build/openarm_can/CMakeFiles/openarm-demo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/openarm-demo.dir/examples/demo.cpp.o"
+  "CMakeFiles/openarm-demo.dir/examples/demo.cpp.o.d"
+  "openarm-demo"
+  "openarm-demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/openarm-demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
